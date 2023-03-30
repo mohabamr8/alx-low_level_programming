368168d0b29f58c9c165906381a19a91f57e4a45
mohab <mohabamro905@gmail.com>
2023-03-30 15:41:07 -0700

    ;

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -23,29 +23,29 @@ void print_buffer(char *b, int size)
 	{
 		j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
-	for (i = 0; i < 10; i++)
+		for (i = 0; i < 10; i++)
 		{
-	if (i < j)
+			if (i < j)
 				printf("%02x", *(b + o + i));
-	else
+			else
 				printf("  ");
-	if (i % 2)
-		{
-			printf(" ");
-		}
+			if (i % 2)
+			{
+				printf(" ");
+			}
 		}
 		for (i = 0; i < j; i++)
 		{
-		int c = *(b + o + i);
+			int c = *(b + o + i);
 
-		if (c < 32 || c > 132)
+			if (c < 32 || c > 132)
 			{
-	c = '.';
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
 	}
-	printf("%c", c);
-	}
-	printf("\n");
-	o += 10;
-}
 }
 
